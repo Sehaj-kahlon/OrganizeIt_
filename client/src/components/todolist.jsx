c0212183e92cafb9ca6e94fd29a83b5396edb078
Sehaj-kahlon <sehajkahlon437@gmail.com>
2024-10-28 00:37:11 +0530

    working on todolist

--- client/src/components/todolist.jsx
+++ client/src/components/todolist.jsx
@@ -1,9 +1,22 @@
 import React from "react";
-// import img from "../assets/todolist.png";
+import { Divider } from "antd";
 import "./todolist.css";
 function Todolist() {
+  const addRow = () => {
+    console.log("Add row");
+  };
   return (
-    <div className="todolist">{/* <p>this is a para elemtent </p> */}</div>
+    <div className="boxelement">
+      <div className="heading">
+        Today's To Do
+        <Divider />
+      </div>
+      <div className="sub-heading">
+        Add to your list
+        <i className="fa-light fa-plus add" onClick={addRow}></i>
+      </div>
+      <Divider />
+    </div>
   );
 }
 
