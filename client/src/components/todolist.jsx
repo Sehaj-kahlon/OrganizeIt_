05c10c83ac5f5390d951bb24eef18d57823a298f
Sehaj-kahlon <sehajkahlon437@gmail.com>
2024-10-29 23:36:27 +0530

    add button

--- client/src/components/todolist.jsx
+++ client/src/components/todolist.jsx
@@ -1,10 +1,22 @@
-import React from "react";
-import { Divider } from "antd";
+import React, { useState } from "react";
+import { Divider, Button, Input } from "antd";
+import { PlusOutlined, DeleteOutlined } from "@ant-design/icons";
 import "./todolist.css";
 function Todolist() {
+  const [rows, setRows] = useState([]);
+  const [idCounter, setIdCounter] = useState(1);
   const addRow = () => {
-    console.log("Add row");
+    setIdCounter(idCounter + 1);
+    console.log(idCounter);
+    setRows([...rows, { id: idCounter, text: "" }]);
   };
+  const deleteRow = (id) => {
+    setRows(rows.filter((row) => row.id !== id));
+  };
+  const handleInputChange = (id, value) => {
+    setRows(rows.map((row) => (row.id === id ? { ...row, text: value } : row)));
+  };
+
   return (
     <div className="boxelement">
       <div className="heading">
@@ -13,9 +25,27 @@ function Todolist() {
       </div>
       <div className="sub-heading">
         Add to your list
-        <i className="fa-light fa-plus add" onClick={addRow}></i>
+        <Button icon={<PlusOutlined />} onClick={addRow}></Button>
       </div>
       <Divider />
+      <div className="Row">
+        {rows.map((row) => (
+          <div key={row.id} className="row">
+            <input type="checkbox" className="checkbox" />
+            <Input
+              value={row.text}
+              onChange={(e) => handleInputChange(row.id, e.target.value)}
+              placeholder="Enter task"
+              className="input"
+            />
+            <Button
+              icon={<DeleteOutlined />}
+              onClick={() => deleteRow(row.id)}
+              className="delete"
+            />
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
